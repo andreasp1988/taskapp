0a109affd4ba8389fa06a3f638b5185bccf47f2a
ledemkam <81742990+ledemkam@users.noreply.github.com>
2021-08-19 14:17:17 +0200

    io

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -4,9 +4,9 @@ import "../css/Buttons.css"
 export default function Buttons() {
    return (
       <div className="buttons">
-         <button className="btn-home1">Ausstehend</button>
-         <button className="btn-home2">Fertig</button>
-         <button className="btn-home3">In Bearbeitung</button>
+         <button className="btn-home1">Ausstehend</button><br/>
+         <button className="btn-home2">Fertig</button><br/>
+         <button className="btn-home3">In Bearbeitung</button><br/>
       </div>
    );
 }
