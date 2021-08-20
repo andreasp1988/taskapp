aa56c21bac6e644b7d94803b27ac07242c105544
Svitlana-G <svitlana.golubieva@gmail.com>
2021-08-20 16:14:21 +0200

    Added DatePicker

--- frontend/src/components/AddAufgabe.js
+++ frontend/src/components/AddAufgabe.js
@@ -6,32 +6,40 @@ import moment from 'moment';
 import TimePicker from 'rc-time-picker';
 import 'rc-time-picker/assets/index.css';
 
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const AddAufgabe = () => {
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <>
             <div className="addAufgabe">
                 <h1>Aufgabe hinzufügen</h1>
+
                 <form action="">
-                    <div>
+                    <div className="flex">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="" id="name" placeholder="Kleiderschrank ausmisten" />
                     </div>
-                    <div>
+                    <div className="flex">
                         <label htmlFor="datum">Datum</label>
-                        <input type="text" name="" id="datum" placeholder="27.08.2021" />
+                        {/* <input type="text" name="" id="datum" placeholder="27.08.2021" /> */}
+                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
                     </div>
+
                     <section className="startEnde">
-                        <div>
+                        <div className="flex">
                             <label htmlFor="start">Start</label>
                             <TimePicker defaultValue={moment()} showSecond={false} minuteStep={15} />
                         </div>
-                        <div>
+                        <div className="flex">
                             <label htmlFor="ende">Ende</label>
                             <TimePicker defaultValue={moment()} showSecond={false} minuteStep={15} />
                         </div>
                     </section>
-                    <div>
+                    <div className="flex">
                         <label htmlFor="descript">Beschreibung</label>
                         <input type="text" name="" id="descript" placeholder="Beschreibung der Tätigkeit" />
                     </div>
