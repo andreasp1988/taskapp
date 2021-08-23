f4149575c0556ecd900effb55bea9323cd0bb19f
Svitlana-G <svitlana.golubieva@gmail.com>
2021-08-23 10:25:28 +0200

    Added model aufgabe to the backend

---
+++ backend/models/aufgabe.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const aufgabeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    datum: {
+        type: Date,
+        required: true
+    },
+    start: {
+        type: String,
+        required: true
+    },
+    ende: {
+        type: String,
+        required: true
+    },
+    beschreibung: {
+        type: String,
+        required: true
+    },
+    kategorie: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const Aufgabe = mongoose.model('aufgabe', aufgabeSchema)
+
+module.exports = Aufgabe
--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/user')
+// const User = require('../models/user')
+
+const Aufgabe = require('../models/aufgabe')
+
+
+router.get('/', (req, res) => {
+    Aufgabe.find()
+        .then(result => res.json(result))
+        .catch(err => console.log(err))
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+})
+
 
 
 router.get('/', (req, res) => {
