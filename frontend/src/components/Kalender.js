88e180bb0fed6bc11c85ad5f37f1aa22833ff91f
andreasp1988 <pocher.andreas1988@gmail.com>
2021-08-26 08:54:16 +0200

    backen8

--- frontend/src/components/Kalender.js
+++ frontend/src/components/Kalender.js
@@ -56,7 +56,7 @@ const Kalender = () => {
             ).map(ele =>
                JSON.stringify(ele.name))
             )} */}
-
+            {date.toLocaleDateString('ru-RU')}
             {data && (data.filter((aufgabe) => aufgabe.datum === date.toLocaleDateString('ru-RU')
             ).map(ele => <div key={ele._id}>
                {ele.name}
