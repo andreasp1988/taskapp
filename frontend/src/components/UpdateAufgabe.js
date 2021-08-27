45f95dc72295295ce47d1e95b8ba8093dad85198
Svitlana-G <svitlana.golubieva@gmail.com>
2021-08-27 12:24:07 +0200

    Update funktioniert

--- frontend/src/components/UpdateAufgabe.js
+++ frontend/src/components/UpdateAufgabe.js
@@ -1,4 +1,3 @@
-import ButtonAddUpd from "./ButtonAddUpd";
 import Nav from "./Nav"
 import '../css/AddUpdAufgabe.css'
 
@@ -15,7 +14,7 @@ import 'rc-time-picker/assets/index.css';
 
 const UpdateAufgabe = () => {
     const [inputs, setInputs] = useState({ name: "", datum: "", start: "", ende: "", beschreibung: "", kategorie: "" })
-    // const [startDate, setStartDate] = useState(new Date());
+
     const [aufgabe, setAufgabe] = useState(null)
 
     let { id } = useParams()
@@ -24,12 +23,18 @@ const UpdateAufgabe = () => {
     useEffect(() => {
         if (!aufgabe) {
             axios.get(`/api/aufgabe/${id}`)
-                .then(result => setAufgabe(result.data))
+                .then(result => {
+                    setAufgabe(result.data)
+                    setInputs(result.data)
+                    console.log(inputs)
+                })
                 .catch((err) => console.log(err))
         }
     })
 
     const handleInputs = (event) => {
+        console.log(event)
+        console.log(inputs)
         setInputs(prev => {
             return {
                 ...prev,
@@ -38,10 +43,9 @@ const UpdateAufgabe = () => {
         })
     }
 
-    // const handleDateInput = (date) => {
-    //     setStartDate(date)
-    //     setInputs({ ...inputs, datum: date.toLocaleDateString('de-DE') })
-    // }
+    const handleDateInput = (date) => {
+        setInputs({ ...inputs, datum: date.toLocaleDateString('de-DE') })
+    }
 
     const handleTimeStartInput = (value) => {
         const hour = value._d.getHours() < 10 ? "0" + value._d.getHours() : value._d.getHours()
@@ -57,72 +61,75 @@ const UpdateAufgabe = () => {
 
     const handleCategoryInput = (e) => {
         setInputs({ ...inputs, kategorie: e.target.value })
+        setAufgabe({ ...aufgabe, kategorie: e.target.value })
     }
 
-    // const updateAufgabe = (inputs) => {
-
-    //     axios.put(`/api/aufgabe/${id}`, inputs)
-    //         .then(result => window.location.href = result.data.redirect)
-    //         .catch(err => console.log(err))
-    // }
+    const updateAufgabe = () => {
+        console.log()
+        axios.put(`/api/aufgabe/${id}`, inputs)
+            .then(result => window.location.href = result.data.redirect)
+            .catch(err => console.log(err))
+    }
 
 
     return (
-        <div className="addAufgabe">
-            <h1>Aufgabe bearbeiten</h1>
-            {aufgabe && (
-                <form >
-                    <div className="flex">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" value={aufgabe.name} onChange={handleInputs} />
-                    </div>
-                    <div className="flex">
-                        <label htmlFor="datum">Datum</label>
-                        <DatePicker className="bg" value={aufgabe.datum} />
-                    </div>
-                    <section className="startEnde">
+        <>
+            <div className="addAufgabe">
+                <h1>Aufgabe bearbeiten</h1>
+                {aufgabe && (
+                    <form >
                         <div className="flex">
-                            <label htmlFor="start">Start</label>
-                            <TimePicker className="bgZeit" defaultValue={moment({ hour: aufgabe.start.substr(0, 2), minute: aufgabe.start.substr(3, 2) })} showSecond={false} minuteStep={15} onChange={handleTimeStartInput} />
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" id="name" defaultValue={aufgabe.name} onChange={handleInputs} />
                         </div>
                         <div className="flex">
-                            <label htmlFor="ende">Ende</label>
-                            <TimePicker defaultValue={moment({ hour: aufgabe.ende.substr(0, 2), minute: aufgabe.ende.substr(3, 2) })} showSecond={false} minuteStep={15} onChange={handleTimeEndeInput} />
+                            <label htmlFor="datum">Datum</label>
+                            <DatePicker className="bg" value={aufgabe.datum} onChange={(date) => handleDateInput(date)} />
                         </div>
-                    </section>
-                    <div className="flex">
-                        <label htmlFor="descript">Beschreibung</label>
-                        <input type="text" name="beschreibung" id="descript" value={aufgabe.beschreibung} onChange={handleInputs} />
-                    </div>
-                    <section className="category">
-                        <label>Kategorie</label>
-                        <section>
-                            {aufgabe.kategorie === "Ausstehend" ? (
-                                <input type="radio" name="kategorie" id="ausstehend" value="Ausstehend" checked onClick={handleCategoryInput} />
-                            ) :
-                                <input type="radio" name="kategorie" id="ausstehend" value="Ausstehend" onClick={handleCategoryInput} />}
-                            <label htmlFor="ausstehend">Ausstehend</label>
+                        <section className="startEnde">
+                            <div className="flex">
+                                <label htmlFor="start">Start</label>
+                                <TimePicker className="bgZeit" defaultValue={moment({ hour: aufgabe.start.substr(0, 2), minute: aufgabe.start.substr(3, 2) })} showSecond={false} minuteStep={15} onChange={handleTimeStartInput} />
+                            </div>
+                            <div className="flex">
+                                <label htmlFor="ende">Ende</label>
+                                <TimePicker defaultValue={moment({ hour: aufgabe.ende.substr(0, 2), minute: aufgabe.ende.substr(3, 2) })} showSecond={false} minuteStep={15} onChange={handleTimeEndeInput} />
+                            </div>
+                        </section>
+                        <div className="flex">
+                            <label htmlFor="descript">Beschreibung</label>
+                            <input type="text" name="beschreibung" id="descript" defaultValue={aufgabe.beschreibung} onChange={handleInputs} />
+                        </div>
+                        <section className="category">
+                            <label>Kategorie</label>
+                            <section>
+                                {aufgabe.kategorie === "Ausstehend" ? (
+                                    <input type="radio" name="kategorie" id="ausstehend" value="Ausstehend" checked onClick={handleCategoryInput} />
+                                ) :
+                                    <input type="radio" name="kategorie" id="ausstehend" value="Ausstehend" onClick={handleCategoryInput} />}
+                                <label htmlFor="ausstehend">Ausstehend</label>
 
-                            {aufgabe.kategorie === "Fertig" ? (
-                                <input type="radio" name="kategorie" id="fertig" value="Fertig" checked onClick={handleCategoryInput} />
-                            ) :
-                                <input type="radio" name="kategorie" id="fertig" value="Fertig" onClick={handleCategoryInput} />}
-                            <label htmlFor="fertig">Fertig</label>
+                                {aufgabe.kategorie === "Fertig" ? (
+                                    <input type="radio" name="kategorie" id="fertig" value="Fertig" checked onClick={handleCategoryInput} />
+                                ) :
+                                    <input type="radio" name="kategorie" id="fertig" value="Fertig" onClick={handleCategoryInput} />}
+                                <label htmlFor="fertig">Fertig</label>
+                            </section>
+                            <section>
+                                {aufgabe.kategorie === "In Bearbeitung" ? (
+                                    <input type="radio" name="kategorie" id="bearbeitung" value="In Bearbeitung" checked onClick={handleCategoryInput} />
+                                ) :
+                                    <input type="radio" name="kategorie" id="bearbeitung" value="In Bearbeitung" onClick={handleCategoryInput} />}
+                                <label htmlFor="bearbeitung">In Bearbeitung</label>
+                                <div><img src="/img/plus.png" alt="" /></div>
+                            </section>
                         </section>
-                        <section>
-                            {aufgabe.kategorie === "In Bearbeitung" ? (
-                                <input type="radio" name="kategorie" id="bearbeitung" value="In Bearbeitung" checked onClick={handleCategoryInput} />
-                            ) :
-                                <input type="radio" name="kategorie" id="bearbeitung" value="In Bearbeitung" onClick={handleCategoryInput} />}
-                            <label htmlFor="bearbeitung">In Bearbeitung</label>
-                            <div><img src="/img/plus.png" alt="" /></div>
-                        </section>
-                    </section>
-                </form>
-            )}
-            <ButtonAddUpd name="Update" />
+                    </form>
+                )}
+                <button onClick={updateAufgabe}>Update </button>
+            </div>
             <Nav />
-        </div>
+        </>
     );
 }
 
